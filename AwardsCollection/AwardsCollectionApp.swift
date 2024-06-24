3def905e238d2f62cc1c76e8bf3d9b939e7cbb8b
Zininas <zininas@mail.ru>
2024-06-24 17:35:31 +0300

    Initial Commit

---
+++ AwardsCollection/AwardsCollectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  AwardsCollectionApp.swift
+//  AwardsCollection
+//
+//  Created by Александр Зинин on 24.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct AwardsCollectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
